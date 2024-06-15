6fd3f67722dfc91d50844b1241115a39f2111b41
曾玉彬 <yuki@hitamaakirasair.lan>
2024-06-15 21:44:15 +1000

    push other files updated too

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862d765c821e230000340d91bc4c3e405fa20b01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a42c322e8e49decc64ac19cdd22d2330b41db97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
